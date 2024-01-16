e1c5300fcc5e1a40c349488e6cd22a0eb7ea94b2
SuzukiHiroto08 <eyc69652@kwansei.ac.jp>
2024-01-16 20:14:21 +0900

    Update file

--- hiroto_push.rb
+++ hiroto_push.rb
@@ -17,7 +17,7 @@ unless FILE_PATH
 end
 
 # リモートリポジトリのURL
-REMOTE_REPO_URL = "https://github.com/SuzukiHiroto08/practice.git"
+REMOTE_REPO_URL = "git@github.com:SuzukiHiroto08/practice.git"
 
 # 現在のディレクトリに移動
 Dir.chdir(File.expand_path('~/grad_research_23/senior/suzuki/practice'))
